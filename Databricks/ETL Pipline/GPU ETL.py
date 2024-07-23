42db724fc3aaec2c91c81c56d45c7d0296f3b8cc
emmy-1 <emmy-1>
2024-07-23 13:28:31 +0000

    updated ETl fifle with a extract_number function

--- Databricks/ETL Pipline/GPU ETL.py
+++ Databricks/ETL Pipline/GPU ETL.py
@@ -11,6 +11,11 @@ def extract_number(df, column_name):
     new_df = df.withColumn(column_name, regexp_extract(df[column_name], pattern, 0))
     return new_df
 
+def extract_number2(df, column_name):
+    pattern = r'(\d{1,3}(?:,\d{3})*(?:\.\d+)?)'  # Regex pattern to extract numeric values
+    new_df = df.withColumn(column_name, regexp_extract(df[column_name], pattern, 1))
+    new_df = new_df.withColumn(column_name, regexp_replace(new_df[column_name], ',', ''))
+    return new_df
 
 def Create_Database(DataFrame, Name):
     SQLDB = DataFrame.createOrReplaceTempView(Name)
@@ -30,13 +35,16 @@ def Run_pipline(table_name):
         WHERE rn = 1
     """
     data = spark.sql(query)
-    Data2 = extract_number(data, 'Ratings')
-    return Data2
+    extract_rating_no = extract_number(data, 'Ratings')
+    extract_tips_no = extract_number2(extract_rating_no, 'Tips')
+    extract_memory_no = extract_number(extract_tips_no, 'Memory')
+    extract_PSU_no = extract_number(extract_memory_no, 'PSU')
+    extract_Length_no = extract_number(extract_PSU_no, 'Length')
+    return extract_Length_no
 
 Result = Run_pipline('Gpu')
+Result.coalesce(1).write.mode("overwrite").csv('abfss://pcpart@neweggdb.dfs.core.windows.net/Dataset/DLT_Bronze_layer/', header=True)
+
+# COMMAND ----------
+
 display(Result)
-
-
-
-    
-
